23c6ea99654405b222d61d4a0937d4028e6264e4
Julien Tachoires <julmon@gmail.com>
2023-02-07 21:33:35 +0100

    AWS TPCC Benchmark against 2 PGD nodes
    Environment variables:
    
    * BENCHMARK_NAME - Directory name used to store benchmark data on the
      S3 bucket
    * TERRAFORM_PROJECT_PATH - Terraform project path
    * EDB_REPO_USERNAME - EDB Package repository username
    * EDB_REPO_PASSWORD - EDB Package repository password
    * TPA_2Q_SUBSCRIPTION_TOKEN - EDB TPAExec subscription key
    * TPCC_WAREHOUSE - Number of TPC-C Warehouse
    * TPCC_DURATION - Test duration in minute (5 min)
    * TPCC_RAMPUP - Rampup duration in minutes (1 min)
    * TPCC_LOADER_VUSERS - Number of vuser used to generate data
    * TPCC_MIN_VUSERS - Minimum number of vuser
    * TPCC_MAX_VUSERS - Maximum number of vuser
    * TPCC_STEP_VUSERS - Number of vuser we add for each iteration
    
    Note: TPAexec must be installed using the following procedure.
    
    $ sudo apt update -y
    $ sudo apt upgrade -y
    $ sudo apt install curl gnupg2 -y
    $ curl https://techsupport.enterprisedb.com/api/repository/<tpa-subscription-key>/products/tpa/release/deb | sudo bash
    $ sudo apt install tpaexec -y
    $ sudo /opt/EDB/TPA/bin/tpaexec setup

---
+++ aws-tpcc-pgd/deploy/run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -eux
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SSH_USER=rocky
+
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+# Create the TPA directory
+mkdir -p ${SCRIPT_DIR}/tpa
+
+python3 ${SCRIPT_DIR}/build-inventory.py ${TERRAFORM_PROJECT_PATH}
+mv ${SCRIPT_DIR}/inventory.yml ${SCRIPT_DIR}/../.
+
+# TPA configuration
+TPA_BIN_DIR=/opt/2ndQuadrant/TPA/bin
+
+TPA_DIR=${SCRIPT_DIR}/tpa
+mv ${SCRIPT_DIR}/config.yml ${TPA_DIR}/.
+mv ${SCRIPT_DIR}/edb-repo-creds.txt ${TPA_DIR}/.
+chmod 0600 ${TPA_DIR}/edb-repo-creds.txt
+export EDB_REPO_CREDENTIALS_FILE=${TPA_DIR}/edb-repo-creds.txt
+
+${TPA_BIN_DIR}/tpaexec relink ${TPA_DIR}
+${TPA_BIN_DIR}/tpaexec provision ${TPA_DIR}
+
+# Setup file systems
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	${SCRIPT_DIR}/playbook-setup-fs.yml
+
+# TPA deployment
+${TPA_BIN_DIR}/tpaexec deploy ${TPA_DIR}
+
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	${SCRIPT_DIR}/playbook-deploy.yml
+
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	${SCRIPT_DIR}/playbook-hammerdb-setup.yml
---
+++ aws-tpcc-pgd/prepare/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eux
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SSH_USER=rocky
+
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	-e "tpcc_warehouse=${TPCC_WAREHOUSE}" \
+	-e "tpcc_loader_vusers=${TPCC_LOADER_VUSERS}" \
+	${SCRIPT_DIR}/playbook-tpcc-build-db.yml
+
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	${SCRIPT_DIR}/playbook-post-build-db.yml
