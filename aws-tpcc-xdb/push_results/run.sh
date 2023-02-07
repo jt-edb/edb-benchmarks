08f9c64c25f3b869ae26fc383477dc1fe247b43e
Julien Tachoires <julmon@gmail.com>
2023-02-07 21:35:19 +0100

    AWS TPCC Benchmark against EPAS nodes w/ XDB
    Environment variables:
    
    * BENCHMARK_NAME - Directory name used to store benchmark data on
      the S3 bucket
    * TERRAFORM_PROJECT_PATH - Terraform project path
    * EDB_REPO_USERNAME - EDB Package repository username
    * EDB_REPO_PASSWORD - EDB Package repository password
    * TPCC_WAREHOUSE - Number of TPC-C Warehouse
    * TPCC_DURATION - Test duration in minute (5 min)
    * TPCC_RAMPUP - Rampup duration in minutes (1 min)
    * TPCC_LOADER_VUSERS - Number of vuser used to generate data
    * TPCC_MIN_VUSERS - Minimum number of vuser
    * TPCC_MAX_VUSERS - Maximum number of vuser
    * TPCC_STEP_VUSERS - Number of vuser we add for each iteration

---
+++ aws-tpcc-xdb/push_results/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eux
+# Push pgbench files to the S3 bucket
+BUCKET_NAME="ebac-reports"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Copy infrastructure.yml and vars.yml
+cp -r ${SCRIPT_DIR}/../execute/benchmark_data ${SCRIPT_DIR}/report-data
+
+date=$(date +'%Y-%m-%dT%H:%M:%S')
+
+aws s3 cp ${SCRIPT_DIR}/../infrastructure.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/infrastructure.yml
+aws s3 cp ${SCRIPT_DIR}/../vars.yml s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/vars.yml
+aws s3 cp ${SCRIPT_DIR}/report-data/ s3://${BUCKET_NAME}/${BENCHMARK_NAME}/${date}/report-data --recursive
