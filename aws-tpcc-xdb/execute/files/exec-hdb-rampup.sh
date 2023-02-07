2b9415df81cfd3c2dd274fa137c5c74fc7db9369
Julien Tachoires <julmon@gmail.com>
2023-02-07 21:35:19 +0100

    AWS TPCC Benchmark against EPAS nodes w/ XDB
    Environment variables:
    
    * BENCHMARK_NAME - Directory name used to store benchmark data on
      the S3 bucket
    * TERRAFORM_PROJECT_PATH - Terraform project path
    * EDB_REPO_USERNAME - EDB Package repository username
    * EDB_REPO_PASSWORD - EDB Package repository password
    * TPCC_WAREHOUSE - Number of TPC-C Warehouse
    * TPCC_DURATION - Test duration in minute (5 min)
    * TPCC_RAMPUP - Rampup duration in minutes (1 min)
    * TPCC_LOADER_VUSERS - Number of vuser used to generate data
    * TPCC_MIN_VUSERS - Minimum number of vuser
    * TPCC_MAX_VUSERS - Maximum number of vuser
    * TPCC_STEP_VUSERS - Number of vuser we add for each iteration

---
+++ aws-tpcc-xdb/execute/files/exec-hdb-rampup.sh
@@ -0,0 +1,80 @@
+#!/bin/bash -eu
+
+VUSER_MIN=${TPCC_MIN_VUSERS:=5}
+VUSER_MAX=${TPCC_MAX_VUSERS:=100}
+VUSER_STEP=${TPCC_STEP_VUSERS:=5}
+# Execute HammerDB for 10mins at each number of vuser
+DURATION=${TPCC_DURATION:=10}
+RAMPUP=${TPCC_RAMPUP:=1}
+WAREHOUSE=${TPCC_WAREHOUSE:=2000}
+HAMMERDB_BIN_PATH=/opt/HammerDB-4.6
+PG_SUPERUSER="${PG_SUPERUSER:=enterprisedb}"
+PG_PORT=${PG_PORT:=5444}
+
+RUNTIMER=$(expr ${DURATION} \* 60 + ${RAMPUP} \* 60)
+
+# We must be in HammerDB installation path if we want to execute hammerdbcli
+cd $HAMMERDB_BIN_PATH
+
+echo "vuser,nopm,catchup_time"
+echo "0,0,0"
+
+for ((c=${VUSER_MIN}; c<=${VUSER_MAX}; c=c+${VUSER_STEP}))
+do
+    # Write the runner TCL script including the wanted number of vuser
+    cat << EOF > /tmp/runner-${c}-vuser.tcl
+#!/usr/bin/env tclsh
+
+proc runtimer { seconds } {
+set x 0
+set timerstop 0
+while {!\$timerstop} {
+incr x
+after 1000
+  if { ![ expr {\$x % 60} ] } {
+          set y [ expr \$x / 60 ]
+          puts "Timer: \$y minutes elapsed"
+  }
+update
+if {  [ vucomplete ] || \$x eq \$seconds } { set timerstop 1 }
+    }
+return
+}
+
+dbset db pg
+diset connection pg_host ${PG_HOST}
+diset connection pg_port ${PG_PORT}
+diset tpcc pg_raiseerror true
+diset tpcc pg_superuser ${PG_SUPERUSER}
+diset tpcc pg_count_ware ${WAREHOUSE}
+diset tpcc pg_driver timed
+diset tpcc pg_duration ${DURATION}
+diset tpcc pg_rampup ${RAMPUP}
+diset tpcc pg_dbase ${PG_DBNAME}
+diset tpcc pg_allwarehouse true
+vuset unique 1
+vuset logtotemp 1
+loadscript
+vuset vu ${c}
+vucreate
+vurun
+runtimer ${RUNTIMER}
+vudestroy
+after 5000
+
+exit
+EOF
+    # Trigger a data checkpoint
+    psql -At -h ${PG_HOST} -U ${PG_SUPERUSER} -p ${PG_PORT} "dbname=${PG_DBNAME} sslmode=disable" -c "CHECKPOINT" > /dev/null 2>&1
+    psql -At -h ${PG_HOST} -U ${PG_SUPERUSER} -p ${PG_PORT} "dbname=${PG_DBNAME} sslmode=disable" -c "VACUUM ANALYZE" > /dev/null 2>&1
+    psql -At -h ${PG_HOST2} -U ${PG_SUPERUSER} -p ${PG_PORT} "dbname=${PG_DBNAME} sslmode=disable" -c "CHECKPOINT" > /dev/null 2>&1
+    psql -At -h ${PG_HOST2} -U ${PG_SUPERUSER} -p ${PG_PORT} "dbname=${PG_DBNAME} sslmode=disable" -c "VACUUM ANALYZE" > /dev/null 2>&1
+    # Run HammerDB TPCC workload for a specific number of vuser
+    nopm=$(./hammerdbcli tcl auto /tmp/runner-${c}-vuser.tcl | grep "System achieved" | sed -E "s/^.*achieved ([0-9\.]+).* NOPM.*$/\1/")
+    # Once HammerDB run is over, get the replication catchup time
+    catchup_time=$(psql -At -h ${PG_HOST} -U ${PG_SUPERUSER} -p ${PG_PORT} "dbname=${PG_DBNAME} sslmode=disable" -c "SELECT EXTRACT(epoch from catchup_time) FROM xdb_monitor_repl_catchup_time() WHERE xdb_slot_name LIKE 'xdb%';")
+
+    echo "${c},${nopm},${catchup_time}"
+    rm -f /tmp/runner-${c}-vuser.tcl
+    sleep 5
+done
---
+++ aws-tpcc-xdb/execute/post-processing.py
@@ -0,0 +1,60 @@
+# encoding: utf-8
+
+# Script in charge of plotting NOPM vs catchup_time over vusers
+# It requires pandas and matplot Python libraries.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import re
+
+
+def gen_nopm_catchup_time_chart(title, data_file, dest_file):
+    data = pd.read_csv(data_file)
+    data.rename(columns={"nopm": "NOPM"}, inplace=True)
+    data.rename(columns={"catchup_time": "Catchup Time (s)"}, inplace=True)
+
+    # Get vuser max value
+    max_vuser = int(data.max()['vuser'])
+    max_nopm = int(data.max()['NOPM'])
+    max_catchup_time = int(data.max()['Catchup Time (s)'])
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    data.plot(
+        x='vuser',
+        y='NOPM',
+        ax=ax,
+        grid=True,
+        ylabel="NOPM",
+        title=title,
+        # Set Y limit to max_nopm + 10% of it
+        ylim=(0, max_nopm + (max_nopm / 10)),
+        xlim=(0, max_vuser),
+    )
+    data.plot(
+        x='vuser',
+        y='Catchup Time (s)',
+        ax=ax,
+        grid=False,
+        ylabel="seconds",
+        title=title,
+        # Set Y limit to max_nopm + 10% of it
+        ylim=(0, max_catchup_time + (max_catchup_time / 10)),
+        xlim=(0, max_vuser),
+        secondary_y=True,
+    )
+
+    fig.savefig(dest_file)
+
+
+def main():
+    data_dir_path = os.path.dirname(os.path.realpath(__file__)) + "/benchmark_data"  # noqa
+    gen_nopm_catchup_time_chart(
+        title = "TPROC-C NOPM vs replication catchup time",
+        data_file="%s/xdb_nopm_catchup_time.csv" % data_dir_path,
+        dest_file="%s/xdb-nopm-catchup-time.png" % data_dir_path,
+    )
+
+if __name__ == '__main__':
+    main()
