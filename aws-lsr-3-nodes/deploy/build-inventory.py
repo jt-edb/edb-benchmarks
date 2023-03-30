8f20c3ec2e36f4a649df78627edc8cc8bcfdae6f
Julien Tachoires <julmon@gmail.com>
2023-03-30 15:37:35 +0200

    Logical Streaming Replication - TPROC-C - 3 nodes

---
+++ aws-lsr-3-nodes/deploy/build-inventory.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+
+import argparse
+from pathlib import Path, PurePath
+import os
+import sys
+import yaml
+
+from jinja2 import Environment, FileSystemLoader
+
+
+def render_template(template_name, vars={}):
+
+    current_dir = Path(__file__).parent.resolve()
+    templates_dir = PurePath.joinpath(current_dir, 'templates')
+
+    file_loader = FileSystemLoader(str(templates_dir))
+    env = Environment(loader=file_loader, trim_blocks=True)
+    template = env.get_template(template_name)
+
+    return template.render(**vars)
+
+
+def template(template_name, dest, vars={}):
+    try:
+        content = render_template(template_name + '.j2', vars)
+        with open(dest, 'w') as f:
+            f.write(content)
+    except Exception as e:
+        sys.exit("ERROR: could not render template %s (%s)"
+                 % (template_name, e))
+
+
+def load_yaml(file_path):
+    if not os.path.exists(file_path):
+        sys.exit("ERROR: file %s not found" % file_path)
+    try:
+        with open(file_path) as f:
+            return yaml.load(f.read(), Loader=yaml.CLoader)
+    except Exception as e:
+        sys.exit("ERROR: could not read file %s (%s)" % (file_path, e))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'project_path',
+        metavar='PROJECT_PATH',
+        type=Path,
+        help="Project path.",
+    )
+    parser.add_argument(
+        '--dest',
+        metavar='DEST_DIR',
+        type=Path,
+        help="Inventory destination directory.",
+        required=False,
+        default=Path(__file__).parent.resolve(),
+    )
+    env = parser.parse_args()
+
+    servers = load_yaml(env.project_path / 'servers.yml')
+
+    vardict = dict(
+        servers = servers['servers']['machines']
+    )
+
+    # Build the inventory file
+    template('inventory.yml', env.dest / 'inventory.yml', vardict)
---
+++ aws-lsr-3-nodes/execute/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eux
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SSH_USER=rocky
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+# Run the ramping up benchmark
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	${SCRIPT_DIR}/playbook-setup-sync-repl.yml
+
+# Run the ramping up benchmark
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	-e "tpcc_warehouse=${TPCC_WAREHOUSE}" \
+	-e "tpcc_duration=${TPCC_DURATION}" \
+	-e "tpcc_rampup=${TPCC_RAMPUP}" \
+	-e "tpcc_min_vusers=${TPCC_MIN_VUSERS}" \
+	-e "tpcc_max_vusers=${TPCC_MAX_VUSERS}" \
+	-e "tpcc_step_vusers=${TPCC_STEP_VUSERS}" \
+	-e "terraform_project_path=${TERRAFORM_PROJECT_PATH}" \
+	${SCRIPT_DIR}/playbook-tpcc-run-rampup.yml
