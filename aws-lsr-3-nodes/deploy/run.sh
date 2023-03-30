4b30ff1eae0763d361991e9256de396491598d2c
Julien Tachoires <julmon@gmail.com>
2023-03-30 15:37:35 +0200

    Logical Streaming Replication - TPROC-C - 3 nodes

---
+++ aws-lsr-3-nodes/deploy/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -eux
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+SSH_USER=rocky
+
+export ANSIBLE_PIPELINING=true
+export ANSIBLE_SSH_PIPELINING=true
+export ANSIBLE_HOST_KEY_CHECKING=false
+
+python3 ${SCRIPT_DIR}/build-inventory.py ${TERRAFORM_PROJECT_PATH}
+mv ${SCRIPT_DIR}/inventory.yml ${SCRIPT_DIR}/../.
+
+# Setup file systems
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	${SCRIPT_DIR}/playbook-setup-fs.yml
+
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	-e "repo_username=${EDB_REPO_USERNAME}" \
+	-e "repo_password=${EDB_REPO_PASSWORD}" \
+	${SCRIPT_DIR}/playbook-deploy.yml
+
+ansible-playbook \
+	-u ${SSH_USER} \
+	--private-key ${TERRAFORM_PROJECT_PATH}/ssh-id_rsa \
+	-i ${SCRIPT_DIR}/../inventory.yml \
+	-e "@${SCRIPT_DIR}/../vars.yml" \
+	${SCRIPT_DIR}/playbook-hammerdb-setup.yml
---
+++ aws-lsr-3-nodes/provision/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eux
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+edb-terraform ${TERRAFORM_PROJECT_PATH} ${SCRIPT_DIR}/../infrastructure.yml
+cd ${TERRAFORM_PROJECT_PATH}
+terraform init
+terraform apply -auto-approve
